b3eea54f78e1d334336052964b4928a5d8890a02
Simon Sandahl <sandahl.simon@hotmail.se>
2021-11-10 16:05:02 +0100

    added tasks in list + styling

--- js/main.js
+++ js/main.js
@@ -3,9 +3,17 @@ window.onload = function (){
     onFirstTimeLoadingSite();
 }
 
-
 // Functions
 function onFirstTimeLoadingSite (){
-  
+    // Make functions on click of button
+    let addTaskBtn = document.getElementById("add-items-btn");
+    addTaskBtn.addEventListener("click", clickToAdd);
 }
+
+// Eventlistener functions
+function clickToAdd(){
+}
+
+
+
 

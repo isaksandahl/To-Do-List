d700efe7c9b057ba41ca20183e670c3d0b9fbcff
Simon Sandahl <sandahl.simon@hotmail.se>
2021-11-12 15:49:32 +0100

    change pretty much everything

--- js/main.js
+++ js/main.js
@@ -1,54 +1,119 @@
-// Functions to load on site load
-window.onload = function (){
-    firstTimeLoadingSite();
+class Task {
+    constructor(isDone, taskItem){
+        this.isDone = isDone;
+        this.taskItem = taskItem;
+    }
 }
 
-// Functions
+window.onload = function (){
+    addTasktoList();
+    createHtml();
+}
+
+let listOfTasks = [];
+
+let inputField = document.querySelector("#add-items-input-id");
+
+let toDoLocalStorage = localStorage.getItem("toDoLocalStorage");
+
+if (toDoLocalStorage === null){
+    listOfTasks = [];
+    firstTimeLoadingSite();
+}
+else {
+    listOfTasks = JSON.parse(toDoLocalStorage);
+}
+
+//
+// FUNCTIONS
+//
 function firstTimeLoadingSite(){
-    // get button to add tasks to list
-    let addTaskButton = document.getElementById("add-items-button");
-    addTaskButton.addEventListener("click", clickToAdd);
+    let firstTask = new Task(true, "Wake Up");
+    let secondTask = new Task(false, "Eat Something");
+    let thirdTask = new Task(false, "Go Workout");
+    let fourthTask = new Task(false, "Eat Something Again");
+
+    listOfTasks.push(firstTask, secondTask, thirdTask, fourthTask);
+}
+
+function addTasktoList(){
+    // get button to add tasks
+    const addItemButton = document.querySelector("#add-items-button-id");
+
+    // create eventlistener to add button
+    addItemButton.addEventListener("click", clickToAdd);
+}
+
+function createHtml (){
+    localStorage.setItem("toDoLocalStorage", JSON.stringify(listOfTasks));
+
+    let parentContainer = document.getElementById("task-container-id");
+    let listParentUl = document.getElementById("list-parent");
+    listParentUl.innerHTML = "";
+
+    for (let i = 0; i < listOfTasks.length; i++) {
+        console.log(i);
+ 
+        // create div for added tasks
+        let addedListContainer = document.createElement("div");
+        addedListContainer.className = "list-container";
+        
+        // create checked button for task
+        let checkedButton = document.createElement("button");
+        checkedButton.innerHTML = "<i class='fas fa-check'></i>";
+        checkedButton.className = "checked-btn";
+        checkedButton.addEventListener("click", () => { ifTaskIsComplete(listOfTasks[i]) });
+        addedListContainer.appendChild(checkedButton);
+        
+        if(listOfTasks[i].isDone === true){
+            addedListContainer.classList.add("checked-btn-complete");
+        }
+
+        // create li-task
+        let addedTask = document.createElement("li");
+        addedTask.innerText = listOfTasks[i].taskItem;
+        addedListContainer.appendChild(addedTask);
+        
+        
+        // create remove button for task
+        let removeButton = document.createElement("button");
+        removeButton.innerHTML = "<i class='fas fa-trash-alt'></i>";
+        removeButton.className = "remove-btn";
+        removeButton.addEventListener("click", () => { removeClickedTask(i) });
+        addedListContainer.appendChild(removeButton);
+        
+        // get parent containers and append the list container
+        //let parentContainer = document.getElementById("task-container");
+        listParentUl.appendChild(addedListContainer);
+        parentContainer.appendChild(listParentUl);
+    }
 }
 
 // Eventlistener functions
 function clickToAdd(e){
     e.preventDefault();
 
-    // get the input-field
-    let inputField = document.getElementById("add-items-input");
+    let inputField = document.querySelector("#add-items-input-id");
     
-    // create div for added tasks
-    let addedListContainer = document.createElement("div");
-    addedListContainer.className = "list-container";
+    let newTask = new Task(false, inputField.value);
     
-    // create checked button for task
-    let checkedButton = document.createElement("button");
-    checkedButton.innerHTML = "<i class='fas fa-check'></i>";
-    checkedButton.className = "checked-btn";
-    addedListContainer.appendChild(checkedButton);
-    
-    // create li-task
-    let addedTask = document.createElement("li");
-    addedTask.innerText = inputField.value;
-    addedListContainer.appendChild(addedTask);
-    
-    
-    // create remove button for task
-    let removeButton = document.createElement("button");
-    removeButton.innerHTML = "<i class='fas fa-trash-alt'></i>";
-    removeButton.className = "remove-btn";
-    addedListContainer.appendChild(removeButton);
-    
-    // get parent containers and append the list container
-    let listParentUl = document.getElementById("list-parent");
-    let parentContainer = document.getElementById("task-container");
-    listParentUl.appendChild(addedListContainer);
-    parentContainer.appendChild(listParentUl);
-    
-    // remove value of input field after click
+    listOfTasks.push(newTask);
+
+    if(inputField.value === ""){
+        return false;
+    }
+
     inputField.value = "";
+
+    createHtml();
 }
 
+function ifTaskIsComplete(taskClicked){
+    taskClicked.isDone = !taskClicked.isDone;
+    createHtml();
+}
 
-
-
+function removeClickedTask(i){
+    listOfTasks.splice(i, 1);
+    createHtml();
+}
